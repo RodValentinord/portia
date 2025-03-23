7efaaf789b27d86eea5b0b02dc755add33a56d75
Rodolfo Valentino <rodolfo.valentino@rdstation.com>
2025-03-22 21:07:45 -0300

    api basica de projetos

--- backend/src/projeto/projeto.module.ts
+++ backend/src/projeto/projeto.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { DbModule } from 'src/db/db.module';
+import { ProjetoController } from './projeto.controller';
+import { ProjetoPrisma } from './projeto.prisma';
 
-@Module({})
+@Module({
+    imports:[DbModule],
+    controllers: [ProjetoController],
+    providers: [ProjetoPrisma]
+})
 export class ProjetoModule {}
