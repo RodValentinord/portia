de599ee5c8509e36212ad8c46393ae990f2a4efe
Rodolfo Valentino <rodolfo.valentino@rdstation.com>
2025-03-23 18:34:47 -0300

    core

--- core/src/projeto/Projeto.ts
+++ core/src/projeto/Projeto.ts
@@ -1,15 +1,15 @@
-import Tecnologia from "../tecnologia/Tecnologia";
-import { Nivel } from "./Nivel";
-import { Tipo } from "./Tipo";
+import Tecnologia from "../tecnologia/Tecnologia"
+import { Nivel } from "./Nivel"
+import { Tipo } from "./Tipo"
 
 export default interface Projeto {
-    id: number;
-    nome: string;
-    descricao: string;
-    imagens: string[];
-    nivel: Nivel;
-    tipo: Tipo;
-    tecnologias: Tecnologia[];
-    link?: string;
-    repositorio?: string;
+	id: number
+	nome: string
+	descricao: string
+	imagens: string[]
+	tipo: Tipo
+	nivel: Nivel
+	repositorio: string
+	destaque: boolean
+	tecnologias: Tecnologia[]
 }
